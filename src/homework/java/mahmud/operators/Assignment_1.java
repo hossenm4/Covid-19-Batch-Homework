4038cdb115737a7a2430115474b255000c402cab
Kazi Islam <kislam780@yahoo.com>
2020-09-16 22:40:04 -0500

    File changes from Mahmud

---
+++ src/homework/java/mahmud/operators/Assignment_1.java
@@ -0,0 +1,8 @@
+package homework.java.mahmud.operators;
+
+public class Assignment_1 {
+    public static void main(String[] args) {
+        System.out.println("Hello every one....");
+
+    }
+}
---
+++ src/homework/java/mahmud/variables/Assignment_1.java
@@ -0,0 +1,7 @@
+package homework.java.mahmud.variables;
+
+public class Assignment_1 {
+    public static void main(String[] args) {
+        System.out.println("Hello every one....");
+    }
+}
