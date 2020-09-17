7107d8cd3ddd2c524096d6835d705394a57de51e
md jakir hossen <hossenm3@gmail.com>
2020-09-17 14:42:02 -0400

    intial java package from jakir

---
+++ src/homework/java/Jakir/variables/Hello_java.java
@@ -0,0 +1,8 @@
+package homework.java.Jakir.variables;
+
+public class Hello_java {
+
+    public static void main(String[] args) {
+        System.out.println("Hellow, From MD jakir hossen");
+    }
+}
